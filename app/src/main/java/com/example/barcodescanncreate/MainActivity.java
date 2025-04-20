05416e72eb5e4e6270c17efdae9940ec4c235c2d
ryn <raynrayan1341@gmail.com>
2025-04-20 14:26:38 +0700

    Update Main Activity dan penyesuaian xml agar fullscreen

--- app/src/main/java/com/example/barcodescanncreate/MainActivity.java
+++ app/src/main/java/com/example/barcodescanncreate/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.barcodescanncreate;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    @SuppressLint("NonConstantResourceId")
-    @Override protected void onCreate(Bundle s) {
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    protected void onCreate(Bundle s) {
         super.onCreate(s);
+
+        // Terapkan fullscreen layout
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            );
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_main);
+
         BottomNavigationView nav = findViewById(R.id.bottom_nav);
         nav.setOnItemSelectedListener(item -> {
             Fragment f;
@@ -35,7 +53,18 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
             return true;
         });
-        // tampilkan fragment scan default
+
+        // Tampilkan fragment scan secara default
         nav.setSelectedItemId(R.id.menu_scan);
+    }
+
+    public static void setWindowFlag(Activity activity, int bits, boolean on) {
+        WindowManager.LayoutParams layoutParams = activity.getWindow().getAttributes();
+        if (on) {
+            layoutParams.flags |= bits;
+        } else {
+            layoutParams.flags &= ~bits;
+        }
+        activity.getWindow().setAttributes(layoutParams);
     }
 }
